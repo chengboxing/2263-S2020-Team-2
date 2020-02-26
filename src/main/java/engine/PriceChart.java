d8361701883827ecb5d753e87922e69611a00b8a
Davis Bolt <davisbolt@gmail.com>
2020-02-26 14:41:40 -0700

    implemented price chart

--- src/main/java/engine/PriceChart.java
+++ src/main/java/engine/PriceChart.java
@@ -3,15 +3,39 @@ package java.engine;
 public class PriceChart {
 
     public static int getStockPrice(HotelChainCategory  chainCategory, int chainSize){
-        return -1;
+        int price;
+
+        if (chainSize < 2){
+            price = 0;
+        } else if (chainSize <= 5){
+            price = 100 * chainSize;
+        } else if (chainSize <= 10){
+            price = 600;
+        } else if (chainSize <= 20){
+            price = 700;
+        } else if (chainSize <= 30){
+            price = 800;
+        } else if (chainSize <= 40){
+            price = 900;
+        } else {
+            price = 1000;
+        }
+
+        if (chainCategory == HotelChainCategory.AVERAGE){
+            price += 100;
+        } else if (chainCategory == HotelChainCategory.EXPENSIVE){
+            price += 200;
+        }
+
+        return price;
     }
 
     public static int getFirstBonus(HotelChainCategory chainCategory, int chainSize){
-        return -1;
+        return getStockPrice(chainCategory, chainSize) * 10;
     }
 
     public static int getSecondBonus(HotelChainCategory chainCategory, int chainSize){
-        return -1;
+        return getStockPrice(chainCategory, chainSize) * 5;
     }
 
 
