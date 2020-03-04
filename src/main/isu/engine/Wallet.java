c85af8db4b50e2af785d4a471a0d2443f85b4bc7
Ziming Wang <wangzimi@isu.edu>
2020-03-04 15:00:25 -0600

    Create Wallet class

---
+++ src/main/isu/engine/Wallet.java
@@ -0,0 +1,10 @@
+package isu.engine;
+
+public class Wallet {
+    private int money;
+
+    public void addMoney(int cash){ money += cash; }
+
+    public void pullMoney(int cash){ money -= cash; }
+
+}
