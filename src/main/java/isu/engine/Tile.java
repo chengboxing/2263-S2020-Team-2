7279fc5984c36e960e4085febd671083c40c4cbc
Olga Novichkova <olganovichkova3@gmail.com>
2020-03-12 11:45:03 -0600

    created test classes and added super class in engine

--- src/main/java/isu/engine/Tile.java
+++ src/main/java/isu/engine/Tile.java
@@ -1,30 +1,32 @@
 package isu.engine;
 
-public class Tile {
+public class Tile extends CellLocation{
 
-    private int rowIndex;
-    private int columnIndex;
+    private static int rowIndex;
+    private static int columnIndex;
 
     public Tile(int rowIndex, int columnIndex){
-        this.columnIndex = columnIndex;
-        this.rowIndex = rowIndex;
-
+        super(rowIndex, columnIndex);
     }
 
-    public int getRowIndex() {
-        return rowIndex;
-    }
 
-    public int getColumnIndex() {
-        return columnIndex;
-    }
-
-    public char getRowChar(){
-        return (char)('A' + rowIndex);
-    }
-
-    public String getTileLabel(){
-        return "" + (columnIndex+1) + getRowChar();
-    }
+    /*
+    * This is previous code, keeping just in case.
+    * */
+//    public int getRowIndex() {
+//        return rowIndex;
+//    }
+//
+//    public int getColumnIndex() {
+//        return columnIndex;
+//    }
+//
+//    public char getRowChar(){
+//        return (char)('A' + rowIndex);
+//    }
+//
+//    public String getTileLabel(){
+//        return "" + (columnIndex+1) + getRowChar();
+//    }
 }
 
