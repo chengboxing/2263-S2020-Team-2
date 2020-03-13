f0176ad358658eeefadf8716483d84e49dfbb393
Ziming Wang <wangzimi@isu.edu>
2020-03-12 23:19:48 -0500

    Added CellLocationTest class

---
+++ src/test/java/isu/engine/CellLocationTest.java
@@ -0,0 +1,66 @@
+package isu.engine;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CellLocationTest {
+    private CellLocation fixture;
+
+    public CellLocationTest(){
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        fixture = new CellLocation(2,3);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test getRowIndex method
+     */
+    @Test
+    public void testGetRowIndex() {
+
+        assertEquals(2, fixture.getRowIndex());
+    }
+
+    /**
+     * Test getColumnIndex method
+     */
+    @Test
+    public void testGetColumnIndex(){
+
+        assertEquals(3, fixture.getColumnIndex());
+    }
+    /**
+     * Test getColumnIndex method
+     */
+    @Test
+    public void testGetRowChar(){
+
+        assertEquals('C', fixture.getRowChar());
+    }
+    /**
+     * Test getTileLabel method
+     */
+    @Test
+    public void testGetTileLabel(){
+
+        assertEquals("4C", fixture.getTileLabel());
+    }
+}
