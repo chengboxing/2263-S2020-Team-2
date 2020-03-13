8b03ec6a47f3c3dbadf5a71d2fa85ce5493f818b
Ziming Wang <wangzimi@isu.edu>
2020-03-12 19:20:27 -0500

    Test StockSet class

--- src/test/java/isu/engine/StockSetTest.java
+++ src/test/java/isu/engine/StockSetTest.java
@@ -30,10 +30,44 @@ public class StockSetTest {
     public void tearDown() {
     }
 
-    /**
-     * Test template.
-     */
+    /*
+     * Testing getStock method.
+     *
+     * */
     @Test
-    public void test() {
+    public void testGetStocks() {
+
+        for (int i = 0; i < 7; i++){
+        assertEquals(25, fixture.getStocks(HotelChain.getHotelChains()[i].getName()));
+        }
+
+    }
+    /*
+     * Testing removeStocks method.
+     *
+     * */
+    public void testRemoveStocks(){
+
+        assertEquals(20, fixture.removeStocks(HotelChain.getHotelChains()[0].getName(), 5));
+    }
+    /*
+     * Testing addStocks method.
+     *
+     * */
+    public void testAddStocks(){
+
+        fixture.removeStocks(HotelChain.getHotelChains()[0].getName(), 5);
+
+        assertEquals(23, fixture.addStocks(HotelChain.getHotelChains()[0].getName(), 3));
+
+    }
+    /*
+     * Testing setStocks method.
+     *
+     * */
+    public void testSetStocks(){
+
+        assertEquals(10, fixture.setStocks(HotelChain.getHotelChains()[0].getName(), 10));
+
     }
 }
