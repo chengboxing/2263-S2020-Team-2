c2a45428c189bd40dd5fbb37d297e1fc0f794788
Olga Novichkova <olganovichkova3@gmail.com>
2020-03-27 22:32:24 -0600

    made changed to hotel chain class and started working on game engine

--- src/main/java/isu/engine/Player.java
+++ src/main/java/isu/engine/Player.java
@@ -11,15 +11,12 @@ public class Player {
     private String name;
     private int money;
     private List<Tile> tiles;
-    private HashMap<String, Integer> stocks;
+    private StockSet stocks;
 
     public Player(String name) {
         this.name = name;
         tiles = new ArrayList<>();
-        stocks = new HashMap<>();
-        for (int i = 0; i < 7; i++) {
-            stocks.put(HotelChain.getHotelChains()[i].getName(), 0);
-        }
+        stocks = new StockSet(GameEngine.GAME_ENGINE.getHotelChains(), 0);
     }
 
 
@@ -32,59 +29,44 @@ public class Player {
     }
 
     public void addMoney(int cash) {
-        if(cash <= 0) {
-            throw new IllegalArgumentException("Cannot add 0 or negative amount of money.");
+        if (cash < 0) {
+            throw new IllegalArgumentException("Cannot add negative amount of money.");
         }
         money += cash;
     }
 
-    public void pullMoney(int cash)  {
-
-            money -= cash;
-
+    public void pullMoney(int cash) {
+        if (cash > money) {
+            throw new IllegalArgumentException("not enough funds");
+        }
+        money -= cash;
     }
 
-    private int tileListSize(){
+    private int tileCount() {
         return tiles.size();
     }
 
-    public List<Tile> getTiles(){
-        int index = tileListSize();
-        return tiles;
+    public List<Tile> getTiles() {
+        return Collections.unmodifiableList(tiles);
     }
 
-    public void setTiles(int tileCount, TilePile tilePile) {
+    private void setTiles(TilePile tilePile, int tileCount) {
         for (int i = 0; i < tileCount; i++) {
             tiles.add(tilePile.getRandomTile());
         }
     }
 
-    public void addTile(TilePile tilePile){
+    public void addTile(TilePile tilePile) {
         tiles.add(tilePile.getRandomTile());
     }
 
     public Tile playTile(int tileIndex) {
-       Tile tile = tiles.get(tileIndex);
-       tiles.remove(tileIndex);
-       return tile;
+        Tile tile = tiles.get(tileIndex);
+        tiles.remove(tileIndex);
+        return tile;
     }
 
-    public int addStocks(String chainName, int numStocks){
-        stocks.replace(chainName, stocks.get(chainName) + numStocks);
-        return stocks.get(chainName);
-    }
-
-    public int removeStocks(String chainName, int numStocks){
-        stocks.replace(chainName, stocks.get(chainName) - numStocks);
-        return stocks.get(chainName);
-    }
-
-    public int setStocks(String chainName, int numStocks){
-        stocks.replace(chainName, numStocks);
-        return numStocks;
-    }
-
-    public int getStocks(String chainName){
-        return stocks.get(chainName);
+    public StockSet getStocks() {
+        return stocks;
     }
 }
