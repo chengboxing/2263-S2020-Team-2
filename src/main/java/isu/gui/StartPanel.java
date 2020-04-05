110db6f2b64dbca22913987b54321dac71a130fc
Olga Novichkova <olganovichkova3@gmail.com>
2020-04-05 17:15:34 -0600

    fixed startPanel layout

--- src/main/java/isu/gui/StartPanel.java
+++ src/main/java/isu/gui/StartPanel.java
@@ -2,62 +2,43 @@ package isu.gui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class StartPanel extends JPanel {
 
     MainFrame frame;
-    JPanel lp = new JPanel();
-    JLabel label = new JLabel("Acquire");
-    JPanel panel = new JPanel(new BorderLayout());
-    JPanel btns;
-    JButton btn1 = new JButton("Start New Game");
-    JButton btn2 = new JButton("Load Game");
-    JButton btn3 = new JButton("Exit");
+
 
 
     public StartPanel(MainFrame frame){
         this.frame = frame;
 
-        btns = new JPanel();
-        panel.add(btns, BorderLayout.SOUTH);
-        btns.setLayout(new GridLayout(3, 1));
-        btns.add(btn1);
-        btns.add(btn2);
-        btns.add(btn3);
+        JPanel labelPanel = new JPanel(new GridBagLayout());
+        JPanel buttonPanel = new JPanel(new GridLayout(3, 1));
+//        buttonPanel.setLayout(new GridLayout(3, 1));
+        JLabel label = new JLabel("Acquire");
+        JButton btn1 = new JButton("Start New Game");
+        JButton btn2 = new JButton("Load Game");
+        JButton btn3 = new JButton("Exit");
 
         btn1.addActionListener(e -> frame.showNewGamePanel());
         btn2.addActionListener(e -> frame.showLoadGamePanel());
         btn3.addActionListener(e -> System.exit(0));
 
-        label.setFont(label.getFont ().deriveFont (84.0f));
-        add(label, BorderLayout.CENTER);
-
-        panel.setLayout(new GridLayout());
-        panel.add(label, BorderLayout.CENTER);
+        label.setFont(label.getFont ().deriveFont (108.0f));
 
 
 
+        setLayout(new BorderLayout());
 
-        add(panel);
-        setBackground(Color.WHITE);
+        labelPanel.add(label);
+        labelPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
+        buttonPanel.add(btn1);
+        buttonPanel.add(btn2);
+        buttonPanel.add(btn3);
 
-//        JButton btn1 = new JButton("Start New Game");
-//        btn1.addActionListener(e -> frame.showNewGamePanel());
-//        add(btn1, BorderLayout.SOUTH);
-//
-//        JButton btn2 = new JButton("Load Game");
-//        btn2.addActionListener(e -> frame.showLoadGamePanel());
-//        add(btn2, BorderLayout.AFTER_LAST_LINE);
-//
-//        JButton btn3 = new JButton("Exit");
-//        btn3.addActionListener(e -> System.exit(0));
-//        add(btn3, BorderLayout.AFTER_LAST_LINE);
-
-
-
+        add(labelPanel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
 
 
     }
