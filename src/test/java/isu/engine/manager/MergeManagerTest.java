90d82db26286e60c2c74f9fb7a9082803e74ec28
Ziming Wang <wangzimi@isu.edu>
2020-04-24 16:52:06 -0500

    Added MergeManager class

---
+++ src/test/java/isu/engine/manager/MergeManagerTest.java
@@ -0,0 +1,39 @@
+package isu.engine.manager;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MergeManagerTest {
+
+
+    public MergeManagerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test
+     */
+    @Test
+    public void test() {
+    }
+}
