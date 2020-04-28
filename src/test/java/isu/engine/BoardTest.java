3ed06d214b63e525f11596f411e5003e8a733698
Ziming Wang <wangzimi@isu.edu>
2020-04-28 11:49:40 -0500

    Added some tests for Board class

--- src/test/java/isu/engine/BoardTest.java
+++ src/test/java/isu/engine/BoardTest.java
@@ -1,5 +1,6 @@
 package isu.engine;
 
+import isu.engine.manager.MergeManager;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -7,8 +8,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.rules.ExpectedException;
-
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.List;
 
 public class BoardTest {
     private Board fixture;
@@ -69,10 +71,8 @@ public class BoardTest {
      */
     @Test
     public void testGetCell_3() {
-        System.out.println(fixture.getCell('A', 0));
-        //System.out.println(fixture.getCell("A1"));
-
-        //assertEquals(fixture.getCell('A', 0), fixture.getCell("A0"));
+        assertSame(fixture.getCell('A', 11), fixture.getCell("A11"));
+        assertSame(fixture.getCell('A', 1), fixture.getCell("A1"));
     }
     /**
      * Test placeTile() method.
@@ -80,7 +80,12 @@ public class BoardTest {
     @Test
     public void testPlaceTile() {
 
-        fixture.placeTile(tile1);
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 6; j++){
+                fixture.placeTile(new Tile(i,j));
+            }
+        }
+        assertEquals(78, fixture.getEmptyCells().size());
 
     }
     /**
@@ -89,68 +94,105 @@ public class BoardTest {
     @Test
     public void testGetEmptyCells() {
         assertEquals(108, fixture.getEmptyCells().size());
+
         fixture.placeTile(tile1);
         assertEquals(107, fixture.getEmptyCells().size());
     }
     /**
-     * Test getOccupiedCells() method.
+     * Test getOccupiedCells() method,
+     * when tile's number is 1
      */
     @Test
-    public void testGetOccupiedCells() {
+    public void testGetOccupiedCells_0() {
         assertEquals(0, fixture.getOccupiedCells().size());
         fixture.placeTile(tile1);
         assertEquals(1, fixture.getOccupiedCells().size());
     }
     /**
-     * Test getNeighboringTiles() method.
+     * Test getOccupiedCells() method,
+     * when tiles' number is larger than 1
+     */
+    @Test
+    public void testGetOccupiedCells_1() {
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 6; j++){
+                fixture.placeTile(new Tile(i,j));
+            }
+        }
+        assertEquals(30, fixture.getOccupiedCells().size());
+    }
+    /**
+     * Test getNeighboringTiles() method,
+     * Test tile1's neighboring tiles
      */
     @Test
         public void testGetNeighboringTiles() {
+
         fixture.placeTile(tile1);
         assertEquals(0, fixture.getNeighboringTiles(tile1).size());
+        fixture.placeTile(tile2);
+        fixture.placeTile(tile4);
 
-        fixture.placeTile(new Tile(1, 0));
-        fixture.placeTile(new Tile(2, 1));
         assertEquals(2, fixture.getNeighboringTiles(tile1).size());
-
+        assertEquals(tile2, fixture.getNeighboringTiles(tile1).get(0));
+        assertEquals(tile4, fixture.getNeighboringTiles(tile1).get(1));
     }
     /**
-     * Test getNeighboringChains() method.
+     * Test getNeighboringChains() method.,
+     * When the tile1's neighboring chain has 1
      */
     @Test
     public void testGetNeighboringChains() {
-        assertEquals(0, fixture.getNeighboringTiles(tile1).size());
-        ArrayList<Tile> tiles = new ArrayList<>();
-        tiles.add(tile2);
-        tiles.add(tile3);
 
-        gameEngine.getHotelChains()[0].addTiles(tiles);
-        fixture.placeTile(tile1);
+        assertEquals(0, fixture.getNeighboringChains(tile1).size());
+
+        gameEngine.getHotelChains()[0].addTile(tile2);
+        gameEngine.getHotelChains()[0].addTile(tile3);
         fixture.placeTile(tile2);
+        fixture.placeTile(tile3);
 
         assertEquals(1, fixture.getNeighboringChains(tile1).size());
-
     }
     /**
-     * Test canPlayTile() method.
+     * Test canPlayTile() method,
+     * when there is no safe chain
      */
     @Test
     public void testCanPlayTile_0() {
         ArrayList<Tile> tiles = new ArrayList<>();
         tiles.add(tile2);
         tiles.add(tile3);
-
         gameEngine.getHotelChains()[0].addTiles(tiles);
-        fixture.placeTile(tile1);
         fixture.placeTile(tile2);
+        fixture.placeTile(tile3);
 
         assertTrue(fixture.canPlayTile(tile1));
     }
     /**
-     * Test canPlayTile() method.
+     * Test canPlayTile() method,
+     * when there are two safe chains
      */
     @Test
     public void testCanPlayTile_1() {
+        ArrayList<Tile> firstTiles = new ArrayList<>();
+        for (int a = 0; a < 11; a++){
+            firstTiles.add(new Tile(0,a));
+        }
+        for (int i = 0; i < 11; i++){
+            fixture.placeTile(firstTiles.get(i));
+        }
+        gameEngine.getHotelChains()[0].addTiles(firstTiles);
+
+        ArrayList<Tile> secondTiles = new ArrayList<>();
+        for (int a = 0; a < 11; a++){
+            secondTiles.add(new Tile(2,a));
+        }
+        for (int i = 0; i < 11; i++){
+            fixture.placeTile(secondTiles.get(i));
+        }
+        gameEngine.getHotelChains()[1].addTiles(secondTiles);
+
+        assertFalse(fixture.canPlayTile(tile1));
     }
     /*
      * Testing illegalArgumentException for getCell() method.
@@ -159,5 +201,65 @@ public class BoardTest {
     @Test (expected = ArrayIndexOutOfBoundsException.class)
     public void testGetCellArrayIndexOutOfBoundsException() throws ArrayIndexOutOfBoundsException{
         fixture.getCell('A', -1);
+    }
+    /**
+     * Test createTileChain(HotelChain chain, Tile tile) method,
+     *When I just place tiles and not set chain for these tiles
+     */
+    @Test
+    public void testCreateTileChain_0(){
+
+        fixture.placeTile(tile2);
+        fixture.placeTile(tile3);
+
+        assertNull(tile2.getChain());
+        assertNull(tile3.getChain());
+    }
+    /**
+     * Test createTileChain(HotelChain chain, Tile tile) method,
+     *When I just place tiles and set chain for these tiles
+     */
+    @Test
+    public void testCreateTileChain_1(){
+        fixture.placeTile(tile2);
+        fixture.placeTile(tile3);
+
+        fixture.createTileChain(gameEngine.getHotelChains()[0], tile2);
+        fixture.createTileChain(gameEngine.getHotelChains()[0], tile3);
+
+        assertNotNull(tile2.getChain());
+        assertNotNull(tile3.getChain());
+    }
+    /**
+     * Test tryAddTile(List<Tile> tiles, int ri, int ci) method,
+     * When I have placed a tile(1,1) and use this method to get this tile
+     */
+    @Test
+    public void testTryAddTile_0() throws Exception{
+        ArrayList<Tile> tiles = new ArrayList<>();
+        fixture.placeTile(tile1);
+
+        Class<Board> class1 = Board.class;
+        Method tryAddTile = class1.getDeclaredMethod("tryAddTile", List.class, int.class, int.class);
+        tryAddTile.setAccessible(true);
+        tryAddTile.invoke(fixture, tiles, 1,1);
+
+        assertEquals(1, tiles.size());
+    }
+    /**
+     * Test tryAddTile(List<Tile> tiles, int ri, int ci) method,
+     * When I have placed a tile(1,1), but I use this method to get other tile
+     */
+    @Test
+    public void testTryAddTile_1() throws Exception{
+        ArrayList<Tile> tiles = new ArrayList<>();
+        fixture.placeTile(tile1);
+
+        Class<Board> class1 = Board.class;
+        Method tryAddTile = class1.getDeclaredMethod("tryAddTile", List.class, int.class, int.class);
+        tryAddTile.setAccessible(true);
+        tryAddTile.invoke(fixture, tiles, 0,1);
+
+        assertEquals(0, tiles.size());
     }
 }
